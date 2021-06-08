25a0a8e6203fd29b3f10bbce8f9b0bcc3348b95c
ElvisPoe <elvis.poe96@yahoo.com>
2021-06-08 18:21:46 +0300

    Fixes n Dev

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ToDo/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDo from './components/ToDo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ToDo
   }
 }
 </script>
 
 <style>
+@import url('https://use.fontawesome.com/releases/v5.7.2/css/all.css');
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
+
+body {
+  background: #F0EFE9;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  font-family: 'Open Sans', sans-serif;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Open Sans', sans-serif;
+  font-size: 1rem;
+  color: #333;
+  width: 100%;
+  height: 85vh;
+  margin: auto;
+  max-width: 500px;
+}
+
+p, h1, input, span, div {
+  font-family: 'Open Sans', sans-serif;
 }
 </style>
--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,58 +1,283 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+
+    <div class="lists-container">
+      <div class="todo-list">
+        <h3 class="text-center text-black">TODO</h3>
+        <div class="item" :class="{'show': item.show}" v-for="(item, index) in todoList" :key="index">
+          <div class="item-title">
+            {{ item.title }}
+          </div>
+          <div class="item-manage">
+            <i class="fas fa-trash" @click="deleteItem(item)"></i>
+            <i v-if="!item.complete" class="fas fa-square" @click="completeItem(item)"></i>
+          </div>
+        </div>
+      </div>
+
+      <div class="todo-list complete-list">
+        <div class="show-completed" v-if="completedToDoList.length > 0">
+          <h3 class="text-black">DONE</h3>
+        </div>
+        <div class="item show" v-for="(item, index) in completedToDoList" :key="index">
+          <div class="item-title">
+            {{ item.title }}
+          </div>
+          <div class="item-manage">
+            <i v-if="item.complete" class="fas fa-check-square" @click="uncompleteItem(item)"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div v-show='showAddNewModal' class="add-item">
+      <form @submit.prevent="addItem">
+        <input type="text" placeholder="Add..." v-model="entry">
+      </form>
+      <div class="add-icon" @click="addItem">
+        <i class="fas fa-plus"></i>
+      </div>
+    </div>
+
+    <div class="floating-buttons">
+      <div class="floating-button clear-data" @click="clearData">
+        <i class="fas fa-trash"></i>
+      </div>
+      <div class="floating-button add-new-open" @click='showAddNewModal = !showAddNewModal'>
+        <i v-if="showAddNewModal" class="fas fa-minus"></i>
+        <i v-else class="fas fa-plus"></i>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  // Debug function that runs on build
+  created(){
+    // var exArray = [{id: '123', title: '123', complete: false}];
+    // console.log(exArray);
+    // console.log(localStorage.getItem("todolist"));
+  },
+
+  data: () => {
+    return {
+      showAddNewModal: false,
+      entry: '',
+      // get todolist from storage
+      todoList: localStorage.getItem("todolist") ? JSON.parse(localStorage.getItem("todolist")) : [],
+      // get completedtodolist from storage
+      completedToDoList: localStorage.getItem("completedtodolist") ? JSON.parse(localStorage.getItem("completedtodolist")) : [],
+    }
+  },
+
+  methods: {
+
+    // Add new item
+    addItem() {
+      if(this.entry !== '') {
+        let date = new Date;
+
+        this.todoList.push({
+          id: date.getTime(),
+          title: this.entry,
+          complete: false
+        });
+
+        localStorage.setItem("todolist", JSON.stringify(this.todoList));
+      }
+      this.entry = '';
+    },
+
+    // Clear Local Storage
+    clearData(){
+      this.todoList = [];
+      this.completedToDoList = [];
+      localStorage.clear();
+    },
+
+    // Set an item complete
+    completeItem(item) {
+        item.complete = !item.complete;
+        this.completedToDoList.push(item);
+        let index = this.todoList.findIndex(element => element.id === item.id);
+        this.todoList.splice(index, 1);
+        localStorage.setItem("todolist", JSON.stringify(this.todoList));
+        localStorage.setItem("completedtodolist", JSON.stringify(this.completedToDoList));
+    },
+
+    // Delete an item
+    deleteItem(item){
+      let index = this.todoList.findIndex(element => element.id === item.id);
+      this.todoList.splice(index, 1);
+      this.completedToDoList.splice(index, 1);
+      localStorage.setItem("todolist", JSON.stringify(this.todoList));
+      localStorage.setItem("completedtodolist", JSON.stringify(this.completedToDoList));
+    },
+
+    // Set an item incomplete
+    uncompleteItem(item) {
+        item.complete = !item.complete;
+        let index = this.completedToDoList.findIndex(element => element.id === item.id);
+        this.todoList.push(item);
+        this.completedToDoList.splice(index, 1);
+
+        localStorage.setItem("todolist", JSON.stringify(this.todoList));
+        localStorage.setItem("completedtodolist", JSON.stringify(this.completedToDoList));
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+.container {
+  position: relative;
+  width: calc(100% - 20px);
+  min-height: calc(100% - 20px);
+  background: white;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.lists-container {
+  max-height: 75vh;
+  overflow-y: scroll;
+  padding: 10px;
 }
-li {
-  display: inline-block;
+.floating-button {
+  position: absolute;
+  bottom: 10px;
+  width: 60px;
+  text-align: center;
+  height: 60px;
+  line-height: 60px;
+  background: #50E3A4;
+  border-radius: 50%;
+  color: gray;
+  cursor: pointer;
+  font-size: 18px;
+}
+.floating-button.add-new-open {
+  right: 10px;
+}
+.floating-button.clear-data{
+  left: 10px;
+}
+
+.add-item {
+  display: flex;
+  justify-content: space-between;
+  width: calc(100% - 50px);
+  height: 60px;
+  border-radius: 50px;
+  overflow: hidden;
+  position: absolute;
+  bottom: 80px;
+  left: 0;
+  background: #50e3a4;
+  padding: 0 15px;
   margin: 0 10px;
 }
-a {
-  color: #42b983;
+.add-item form {
+  flex-basis: 100%;
+}
+
+.add-icon {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: gray;
+  font-size: 2rem;
+  cursor: pointer;
+}
+
+input {
+  background: none;
+  border: none;
+  width: 100%;
+  height: 60px;
+  color: black;
+  font-size: 1.6rem;
+}
+
+input::placeholder {
+   color: black;
+}
+
+input:focus {
+   outline: none;
+}
+
+.item {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 60px;
+  margin-bottom: 2px;
+  overflow: hidden;
+  transition: all .5s linear;
+}
+
+.item-manage {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.6rem;
+  color: black;
+  cursor: pointer;
+}
+.item-manage i {
+  padding: 0 15px;
+  font-size: 18px;
+}
+
+.item-title {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  font-size: 1.6rem;
+  color: black;
+}
+
+.complete-list .item {
+  position: relative;
+}
+.complete-list .item::before {
+  content: '';
+  position: absolute;
+  left: 0;
+  top: 50%;
+  width: 90%;
+  margin: 0;
+  border-bottom: 2px solid black;
+ }
+
+.show-completed {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 20px;
+}
+
+.text-center {
+  text-align: center;
+}
+.text-white {
+  color: white;
+}
+.text-black {
+  color: black;
+}
+::-webkit-scrollbar {
+  width: 5px;
+}
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
 }
 </style>
